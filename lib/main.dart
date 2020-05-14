fb6b26e8aa20088cf2f052a769a25da80304f2b6
MuhammadSalmanMehmood <connect_salman@live.com>
2020-05-14 10:41:12 +0500

    With out safearea

---
+++ lib/main.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:audioplayers/audio_cache.dart';
+
+void main() => runApp(XylophoneApp());
+
+class XylophoneApp extends StatelessWidget {
+  void playSound(int soundNumber) {
+    final player = AudioCache();
+    player.play('note$soundNumber.wav');
+  }
+
+  Expanded buildkey({Color color, int soundNumber}) {
+    return Expanded(
+      child: Container(
+        child: FlatButton(
+          color: color,
+          onPressed: () {
+            playSound(soundNumber);
+          }, 
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          child: Center(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                buildkey(color: Colors.red, soundNumber: 1),
+                buildkey(color: Colors.yellow, soundNumber: 2),
+                buildkey(color: Colors.green, soundNumber: 3),
+                buildkey(color: Colors.blue, soundNumber: 4),
+                buildkey(color: Colors.orange, soundNumber: 5),
+                buildkey(color: Colors.pink, soundNumber: 6),
+                buildkey(color: Colors.teal, soundNumber: 7),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
